dcd4e8058f737338f6fe5478bf740f56e6e48d2c
your_github_usernamedsundrud <dsundrud@gmail.com>
2022-09-06 15:56:31 -0700

    added db

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,16 +8,29 @@ def index():
     user = {'username': 'David'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Its a beautiful morning'
+            'author': {'username': 'Ivan'},
+            'body': 'Anybody want some coffee?'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'Love me some deathcore in the morning'
+            'author': {'username': 'Ralph'},
+            'body': 'JUST AS GOOD!'
         },
         {
-            'author': {'username': 'Rodrigo'},
-            'body': 'I think i caught the pox'
+            'author': {'username': 'Carlos'},
+            'body': 'I GOT BIT!!!'
+        },
+        {
+            'author': {'username': 'Adam'},
+            'body': 'STOP!'
         }
     ]
     return render_template('index.html', title = 'Home', user = user, posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
